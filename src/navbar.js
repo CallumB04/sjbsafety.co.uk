69ce525a85506ea22b9d97d89183356db58a2c01
Cal <callumburgoyne04@gmail.com>
2024-12-31 22:57:01 +0000

    feat: add contact navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,8 +1,19 @@
 document.getElementById('navbar').innerHTML = `
-    <nav class="w-screen h-30 fixed top-0 z-10">
-        <div class="w-full h-10 bg-white">
+    <nav class="w-screen fixed top-0 z-10 text-bg-primary">
+        <div class="w-full h-12 bg-highlight-primary justify-center items-center gap-x-12 hidden sm:flex">
+            <span class="flex gap-x-3 items-center">
+                <i class="fas fa-phone text-xl"></i>
+                <p class="text-lg font-semibold">+44 7902 011622</p>
+            </span>
+            <span class="flex gap-x-3 items-center">
+                <i class="fas fa-envelope text-xl"></i>
+                <a href="mailto:sjb.safetyservices@yahoo.com" 
+                   class="text-lg font-semibold">sjb.safetyservices@yahoo.com</a>
+            </span>
         </div>
+
         <div class="w-full h-20 bg-bg-primary">
+            
         </div>
     </nav>
 `;
