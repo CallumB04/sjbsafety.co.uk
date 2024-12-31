bd9fbe54e97766ad3d31efa31c6804c7f3d2da29
Cal <callumburgoyne04@gmail.com>
2024-12-31 23:12:40 +0000

    feat: add navigation bar

--- src/navbar.js
+++ src/navbar.js
@@ -1,6 +1,6 @@
 document.getElementById('navbar').innerHTML = `
     <nav class="w-screen fixed top-0 z-10 text-bg-primary">
-        <div class="w-full h-12 bg-highlight-primary justify-center items-center gap-x-12 hidden sm:flex">
+        <div class="w-full h-12 bg-highlight-primary justify-center items-center gap-x-12 hidden md:flex">
             <span class="flex gap-x-3 items-center">
                 <i class="fas fa-phone text-xl"></i>
                 <p class="text-lg font-semibold">+44 7902 011622</p>
@@ -12,8 +12,16 @@ document.getElementById('navbar').innerHTML = `
             </span>
         </div>
 
-        <div class="w-full h-20 bg-bg-primary">
-            
+        <div class="w-full h-24 md:h-36 bg-bg-primary flex justify-center md:justify-between items-center px-[5%]">
+            <div class="h-full flex items-center">
+                <img src="../assets/logo.png" class="object-cover h-[90%]" />
+            </div>
+            <div class="items-center gap-x-[3vw] text-text-primary font-semibold text-xl hidden md:flex">
+                <a href="" class="">Home</a>
+                <a href="" class="">About us</a>
+                <a href="" class="">Services</a>
+                <a href="" class="">Contact</a>
+            </div>
         </div>
     </nav>
 `;
