2f7afd5a7004f8842ad85936f7ce15211fd1b69f
Cal <callumburgoyne04@gmail.com>
2025-01-02 00:21:17 +0000

    fix: dropdown content hidden when closed

--- src/navbar.js
+++ src/navbar.js
@@ -17,10 +17,10 @@ document.getElementById('navbar').innerHTML = `
                 <img src="../assets/logo.png" class="object-cover h-[90%]" />
             </div>
             <div class="items-center gap-x-[3vw] text-text-primary font-semibold text-xl hidden md:flex">
-                <a href="index.html" class="">Home</a>
-                <a href="about.html" class="">About us</a>
-                <a href="services.html" class="">Services</a>
-                <a href="contact.html" class="">Contact</a>
+                <a href="index.html" class="hover:text-highlight-primary transition-colors duration-200">Home</a>
+                <a href="about.html" class="hover:text-highlight-primary transition-colors duration-200">About us</a>
+                <a href="services.html" class="hover:text-highlight-primary transition-colors duration-200">Services</a>
+                <a href="contact.html" class="hover:text-highlight-primary transition-colors duration-200">Contact</a>
             </div>
             <div class="flex md:hidden absolute right-8 h-full items-center">
                 <div id="phone-dropdown-btn" class="flex flex-col w-9 justify-center gap-y-[6px] p-1 hover:cursor-pointer group">
@@ -29,20 +29,22 @@ document.getElementById('navbar').innerHTML = `
                     <div class="w-full h-[2px] bg-text-primary group-hover:bg-text-secondary"></div>
                 </div>
             </div>
-            <div id="phone-dropdown" class="flex md:hidden flex-col w-9/12 max-w-60 fixed top-24 right-0 bg-highlight-primary
+            <div id="phone-dropdown" class="block md:hidden w-0 max-w-60 fixed top-24 right-0 bg-highlight-primary
                                             transition-[width] duration-500 ease-in-out text-lg">
-                <a href="index.html" class="h-16 flex justify-center items-center w-full whitespace-nowrap
-                                            hover:bg-highlight-secondary hover:text-highlight-primary transition-colors duration-200">Home</a>
-                <a href="about.html" class="h-16 flex justify-center items-center w-full whitespace-nowrap
-                                            hover:bg-highlight-secondary hover:text-highlight-primary transition-colors duration-200">About us</a>
-                <a href="services.html" class="h-16 flex justify-center items-center w-full whitespace-nowrap
-                                               hover:bg-highlight-secondary hover:text-highlight-primary transition-colors duration-200">Services</a>
-                <a href="contact.html" class="h-16 flex justify-center items-center w-full whitespace-nowrap
-                                              hover:bg-highlight-secondary hover:text-highlight-primary transition-colors duration-200">Contact</a>
-                <div class="h-16 flex justify-center gap-x-9 text-[26px] w-full items-center">
-                    <a href="https://www.facebook.com/SJBSafetyservices/" target="_blank" class="fab fa-facebook hover:text-highlight-secondary transition-colors duration-200"></a>
-                    <a href="mailto:sjb.safetyservices@yahoo.com" class="fas fa-envelope hover:text-highlight-secondary transition-colors duration-200"></a>
-                    <a href="tel:+44 7902 011622" class="fas fa-phone hover:text-highlight-secondary transition-colors duration-200 text-[23px]"></a>
+                <div id="phone-dropdown-content" class="w-full h-full flex flex-col opacity-0 transition-opacity duration-300">
+                    <a href="index.html" class="h-16 flex justify-center items-center w-full whitespace-nowrap
+                                                hover:bg-highlight-secondary hover:text-highlight-primary transition-colors duration-200">Home</a>
+                    <a href="about.html" class="h-16 flex justify-center items-center w-full whitespace-nowrap
+                                                hover:bg-highlight-secondary hover:text-highlight-primary transition-colors duration-200">About us</a>
+                    <a href="services.html" class="h-16 flex justify-center items-center w-full whitespace-nowrap
+                                                hover:bg-highlight-secondary hover:text-highlight-primary transition-colors duration-200">Services</a>
+                    <a href="contact.html" class="h-16 flex justify-center items-center w-full whitespace-nowrap
+                                                hover:bg-highlight-secondary hover:text-highlight-primary transition-colors duration-200">Contact</a>
+                    <div class="h-16 flex justify-center gap-x-9 text-[26px] w-full items-center">
+                        <a href="https://www.facebook.com/SJBSafetyservices/" target="_blank" class="fab fa-facebook hover:text-highlight-secondary transition-colors duration-200"></a>
+                        <a href="mailto:sjb.safetyservices@yahoo.com" class="fas fa-envelope hover:text-highlight-secondary transition-colors duration-200"></a>
+                        <a href="tel:+44 7902 011622" class="fas fa-phone hover:text-highlight-secondary transition-colors duration-200 text-[23px]"></a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -51,7 +53,10 @@ document.getElementById('navbar').innerHTML = `
 
 const phoneDropdownBtn = document.getElementById("phone-dropdown-btn");
 const phoneDropdown = document.getElementById("phone-dropdown");
+const phoneDropdownContent = document.getElementById("phone-dropdown-content");
 phoneDropdownBtn.addEventListener("click", () => {
     phoneDropdown.classList.toggle("w-9/12");
     phoneDropdown.classList.toggle("w-0");
+    phoneDropdownContent.classList.toggle("opacity-0");
+    phoneDropdownContent.classList.toggle("opacity-100");
 });
