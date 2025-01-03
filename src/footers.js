630461c42e8da7e1f62de525353164e9beae6335
Cal <callumburgoyne04@gmail.com>
2025-01-03 10:59:44 +0000

    fix: improve accreditations footer styling

--- src/footers.js
+++ src/footers.js
@@ -1,27 +1,27 @@
 document.getElementById('accreditations').innerHTML = `
-    <footer class="w-screen h-80 bg-white flex flex-wrap flex-row items-center justify-evenly px-4">
-        <div class="h-[45%]">
+    <footer class="w-screen bg-white flex flex-wrap flex-row items-center justify-center gap-8 p-4">
+        <div class="h-36">
             <img src="../assets/accreditations/acclaim.jpg" class="h-full object-cover"/>
         </div>
-        <div class="h-[45%]">
+        <div class="h-36">
             <img src="../assets/accreditations/alcumus.jpg" class="h-full object-cover"/>
         </div>
-        <div class="h-[45%]">
+        <div class="h-36">
             <img src="../assets/accreditations/builders-profile.jpg" class="h-full object-cover"/>
         </div>
-        <div class="h-[45%]">
+        <div class="h-36">
             <img src="../assets/accreditations/CFA.png" class="h-full object-cover"/>
         </div>
-        <div class="h-[45%]">
+        <div class="h-36">
             <img src="../assets/accreditations/constructionline.jpg" class="h-full object-cover"/>
         </div>
-        <div class="h-[45%]">
+        <div class="h-36">
             <img src="../assets/accreditations/hydrajaws.jpg" class="h-full object-cover"/>
         </div>
-        <div class="h-[45%]">
+        <div class="h-36">
             <img src="../assets/accreditations/nasc.jpg" class="h-full object-cover"/>
         </div>
-        <div class="h-[45%]">
+        <div class="h-36">
             <img src="../assets/accreditations/ssip.jpg" class="h-full object-cover"/>
         </div>
     </footer>
